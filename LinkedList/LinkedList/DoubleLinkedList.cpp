9b85558189bab4fa12a41952c8942411a226c077
hun2hun2 <aa09302003@gmail.com>
2026-07-24 01:08:46 +0900

    complete RingLinkedList.cpp and DoubleLinkedList.cpp

--- LinkedList/LinkedList/DoubleLinkedList.cpp
+++ LinkedList/LinkedList/DoubleLinkedList.cpp
@@ -17,6 +17,12 @@ struct SNode {
 	SNode* pNext;
 };
 
+struct SDoubleNode {
+	int nData;
+	SDoubleNode* pNext;
+	SDoubleNode* pPre;
+};
+
 SNode* CreateNode(SNode* pNode, int data); //노드를 생성하여 리턴한다.
 SNode* FindNodeData(SNode* pStart, int data); //해당 데이터를 가진 노드를 찾는다.
 SNode* InsertNodeData(SNode* pStart, int data, int insert); //해당 데이터를 가진 노드 뒤에 노드를 추가한다.
@@ -24,6 +30,14 @@ void DeleteNodeData(SNode* pStart, int del); //해당데이터를 가진 노드를 삭제한다
 void PrintLinkedList(SNode* pStart); //노드를 순회하며 끝날때까지 출력한다.
 void DeleteLinkedList(SNode* pStart); //노드를 순회하며 모든데이터를 삭제한다.
 void ReverseLinkedList(SNode* pStart); //
+void DoubleLinkedListMain();
+SDoubleNode* CreateDoubleNode(SDoubleNode* pNode, int data);
+SDoubleNode* InsertDoubleNodeData(SDoubleNode* pStart, int data, int insert);
+void PrintDoubleLinkedList(SDoubleNode* pStart);
+SDoubleNode* FindDoubleNodeData(SDoubleNode* pStart, int data);
+void DeleteDoubleNodeData(SDoubleNode* pStart, int del);
+void DeleteDoubleLinkedList(SDoubleNode* pStart);
+void PrintDoubleLinkedListReverse(SDoubleNode* pEnd);
 
 //연결리스트 동적으로 입력받기.(동적할당 설명용)
 void InputAdd();
@@ -34,43 +48,7 @@ void InputAdd();
 //main()함수 내 코드는 추가는 가능하지만 삭제는 하지말것!
 void main()
 {
-	//_CrtSetBreakAlloc(96); //메모리 누수시 번호를 넣으면 할당하는 위치에 브레이크 포인트를 건다.
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); //메모리 누수 검사 
-
-	SNode* pBegin = NULL;
-	SNode* pEnd = NULL;
-
-	//노드 추가 테스트
-	pEnd = CreateNode(pEnd, 10);
-	pBegin = pEnd; //마지막 노드를 알아야 검색이 가능하므로 저장해둔다.
-
-	pEnd = CreateNode(pEnd, 20);
-	pEnd = CreateNode(pEnd, 30);
-	pEnd = CreateNode(pEnd, 40);
-	pEnd = CreateNode(pEnd, 50);
-
-	PrintLinkedList(pBegin);
-
-	SNode* pFind = FindNodeData(pBegin, 40);
-	if (pFind != NULL)
-		printf("Find:%d\n", pFind->nData);
-
-	pEnd = InsertNodeData(pBegin, 30, 60);//노드 삽입
-
-	PrintLinkedList(pBegin);
-
-	DeleteNodeData(pBegin, 60);//노드 삭제
-
-	PrintLinkedList(pBegin);
-
-	ReverseLinkedList(pBegin);
-	SNode* pTemp = pBegin;
-	pBegin = pEnd;
-	pEnd = pTemp;
-
-	PrintLinkedList(pBegin);
-
-	DeleteLinkedList(pBegin); //모든노드삭제 - 이 함수를 호출하지않을시 메모리가 누수됨.
+	DoubleLinkedListMain();
 }
 
 //여기서 부터 기능을 구현한다.
@@ -169,7 +147,7 @@ void ReverseLinkedList(SNode* pStart)
 {
 	SNode* pNode = pStart;
 	SNode* pPre = NULL;
-	SNode* pAfter = NULL;
+	SNode* pAfter = pNode->pNext;
 
 	while (pNode)
 	{
@@ -209,4 +187,154 @@ void InputAdd()
 	}
 
 	DeleteLinkedList(pStart); //모든노드삭제 - 이 함수를 호출하지않을시 메모리가 누수됨.
+}
+
+void DoubleLinkedListMain()
+{
+	//_CrtSetBreakAlloc(71); //메모리 누수시 번호를 넣으면 할당하는 위치에 브레이크 포인트를 건다.
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); //메모리 누수 검사 
+
+	SDoubleNode* pBegin = NULL;
+	SDoubleNode* pEnd = NULL;
+
+	//노드 추가 테스트
+	pEnd = CreateDoubleNode(pEnd, 10);
+	pBegin = pEnd; //마지막 노드를 알아야 검색이 가능하므로 저장해둔다.
+
+	pEnd = CreateDoubleNode(pEnd, 20);
+	pEnd = CreateDoubleNode(pEnd, 30);
+	pEnd = CreateDoubleNode(pEnd, 40);
+	pEnd = CreateDoubleNode(pEnd, 50);
+
+	PrintDoubleLinkedList(pBegin);
+
+	SDoubleNode* pFind = FindDoubleNodeData(pBegin, 40);
+	if (pFind != NULL)
+		printf("Find:%d\n", pFind->nData);
+
+	pEnd = InsertDoubleNodeData(pBegin, 30, 60);//노드 삽입
+
+	PrintDoubleLinkedList(pBegin);
+
+	DeleteDoubleNodeData(pBegin, 60);//노드 삭제
+
+	PrintDoubleLinkedList(pBegin);
+
+	PrintDoubleLinkedListReverse(pEnd);
+
+	DeleteDoubleLinkedList(pBegin); //모든노드삭제 - 이 함수를 호출하지않을시 메모리가 누수됨.
+}
+
+SDoubleNode* CreateDoubleNode(SDoubleNode* pNode, int data)
+{
+	SDoubleNode* pTemp = NULL;
+
+	pTemp = new SDoubleNode();
+	pTemp->nData = data;
+	pTemp->pNext = NULL;
+	pTemp->pPre = NULL;
+	if (pNode != NULL)
+	{
+		pNode->pNext = pTemp;
+		pTemp->pPre = pNode;
+	}
+
+	return  pTemp;
+}
+
+void PrintDoubleLinkedList(SDoubleNode* pStart)
+{
+	SDoubleNode* pNode = pStart;
+	printf("data:");
+	while (pNode)
+	{
+		printf("%d", pNode->nData);
+		pNode = pNode->pNext;
+
+		if (pNode != NULL)
+			printf(",");
+	}
+	printf("\n");
+}
+
+SDoubleNode* InsertDoubleNodeData(SDoubleNode* pStart, int data, int insert)
+{
+	SDoubleNode* pNode = pStart;
+	SDoubleNode* pInsert = NULL;
+
+	pNode = FindDoubleNodeData(pStart, data);
+
+	pInsert = CreateDoubleNode(pInsert, insert);
+	pInsert->pNext = pNode->pNext;
+	pInsert->pNext->pPre = pInsert;
+	pNode->pNext = pInsert;
+	pInsert->pPre = pNode;
+	while (pNode->pNext) pNode = pNode->pNext;
+
+	return pNode;
+}
+
+SDoubleNode* FindDoubleNodeData(SDoubleNode* pStart, int data)
+{
+	SDoubleNode* pNode = pStart;
+	while (pNode)
+	{
+		if (pNode->nData == data)
+		{
+			return pNode;
+		}
+
+		pNode = pNode->pNext;
+	}
+
+	return pNode;
+}
+
+void DeleteDoubleNodeData(SDoubleNode* pStart, int del)
+{
+	SDoubleNode* pPre = NULL;
+	SDoubleNode* pNode = pStart;
+
+	while (pNode->pNext)
+	{
+		if (pNode->pNext->nData == del)
+		{
+			pPre = pNode;
+			pNode = pNode->pNext;
+			pPre->pNext = pNode->pNext;
+			pNode->pNext->pPre = pPre;
+			delete pNode;
+			return;
+		}
+
+		pNode = pNode->pNext;
+	}
+}
+
+void DeleteDoubleLinkedList(SDoubleNode* pStart)
+{
+	SDoubleNode* pNode = pStart;
+	SDoubleNode* pDel = NULL;
+
+	while (pNode)
+	{
+		pDel = pNode;
+		pNode = pNode->pNext;
+		delete pDel;
+	}
+}
+
+void PrintDoubleLinkedListReverse(SDoubleNode* pEnd)
+{
+	SDoubleNode* pNode = pEnd;
+	printf("data:");
+	while (pNode)
+	{
+		printf("%d", pNode->nData);
+		pNode = pNode->pPre;
+
+		if (pNode != NULL)
+			printf(",");
+	}
+	printf("\n");
 }
