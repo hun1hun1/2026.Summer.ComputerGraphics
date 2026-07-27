0af4de7238169cf0823d08a93e54b331ff6da34e
Jihoon Choi <aa09302003@gmail.com>
2026-07-27 12:50:00 +0900

    add ListStack

--- DataStruct/StackAndQueue/StackAndQueue/Stack.cpp
+++ DataStruct/StackAndQueue/StackAndQueue/Stack.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+struct SNode {
+	int nData;
+	SNode* pNext;
+};
+
 void push(int value);
 int pop();
+SNode* CreateNode(SNode* top, int data);
+SNode* ListPop(SNode* top);
+void ListStackMain();
+void ArrayStackMain();
 
 const int MAX_SIZE = 100;
 int top = -1;
@@ -9,17 +18,7 @@ int stack[MAX_SIZE];
 
 int main()
 {
-	push(10);
-	push(20);
-	push(30);
-	push(40);
-	push(50);
-
-	while (top >= 0)
-	{
-		int value = pop();
-		printf("%d\n", value);
-	}
+	ListStackMain();
 
 	return 0;
 }
@@ -35,4 +34,49 @@ void push(int value)
 int pop()
 {
 	return stack[top--];
+}
+
+SNode* CreateNode(SNode* top, int data)
+{
+	SNode* pTemp = new SNode();
+	pTemp->nData = data;
+	if (top != NULL) pTemp->pNext = top;
+	return pTemp;
+}
+
+SNode* ListPop(SNode* top)
+{
+	printf("%d\n", top->nData);
+	return top->pNext;
+}
+
+void ListStackMain()
+{
+	SNode* pTop = NULL;
+
+	pTop = CreateNode(pTop, 10);
+	pTop = CreateNode(pTop, 20);
+	pTop = CreateNode(pTop, 30);
+	pTop = CreateNode(pTop, 40);
+	pTop = CreateNode(pTop, 50);
+
+	while (pTop != NULL)
+	{
+		pTop = ListPop(pTop);
+	}
+}
+
+void ArrayStackMain()
+{
+	push(10);
+	push(20);
+	push(30);
+	push(40);
+	push(50);
+
+	while (top >= 0)
+	{
+		int value = pop();
+		printf("%d\n", value);
+	}
 }
