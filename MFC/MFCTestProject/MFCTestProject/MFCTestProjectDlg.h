61acfef23b11a776a54dbc85372a53cfb60a9df2
Jihoon Choi <aa09302003@gmail.com>
2026-08-05 12:56:37 +0900

    make MFC Test Projects

---
+++ MFC/MFCTestProject/MFCTestProject/MFCTestProjectDlg.h
@@ -0,0 +1,38 @@
+﻿
+// MFCTestProjectDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCTestProjectDlg 대화 상자
+class CMFCTestProjectDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCTestProjectDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCTESTPROJECT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	// //버튼테스트
+	CButton m_btnButton;
+	afx_msg void OnBnClickedButton1();
+};
