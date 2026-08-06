1cf8b48afc3603263d2ac03e5ba924eb6b59e3fd
hun2hun2 <aa09302003@gmail.com>
2026-08-06 20:13:12 +0900

    make Vector functions and run correctly

--- DataStruct/MakeCustomSTL/MakeCustomSTL/Vector.cpp
+++ DataStruct/MakeCustomSTL/MakeCustomSTL/Vector.cpp
@@ -13,6 +13,45 @@ class Vector
 		int mCapacity;
 
 	public:
+		class iterator
+		{
+			private:
+				T* ptr;
+
+			public:
+				iterator(T* p = nullptr) : ptr(p) {}
+
+				T& operator*() { return *ptr; }
+
+				iterator& operator++()
+				{
+					++ptr;
+					return *this;
+				}
+
+				iterator operator++(int)
+				{
+					iterator temp = *this;
+					++ptr;
+					return temp;
+				}
+
+				bool operator!=(const iterator& other) const
+				{
+					return ptr != other.ptr;
+				}
+
+				bool operator==(const iterator& other) const
+				{
+					return ptr == other.ptr;
+				}
+
+				T* get() const
+				{
+					return ptr;
+				}
+		};
+
 		Vector(int n = 1) : mData(new T[n]), mCapacity(n), mSize(n) 
 		{
 			for (int i = 0; i < n; ++i) mData[i] = T();
@@ -22,6 +61,16 @@ class Vector
 		{
 			for (int i = 0; i < n; ++i) mData[i] = init;
 		};
+
+		iterator begin()
+		{
+			return iterator(mData);
+		}
+
+		iterator end()
+		{
+			return iterator(mData + mSize);
+		}
 
 		void reserve(int newCapacity)
 		{
@@ -60,10 +109,12 @@ class Vector
 
 		void push_back(T data)
 		{
-			if (mCapacity <= mSize + 1)
+			if (mSize >= mCapacity)
 			{
-				mData[mSize++] = data;
+				reserve(mCapacity * 2);
 			}
+
+			mData[mSize++] = data;
 		}
 
 		T& operator[](int i) { return mData[i]; }
@@ -72,65 +123,98 @@ class Vector
 		{
 			for (int i = 0; i < mSize; ++i)
 			{
-				if (data == mData[i]) return i;
-			}
-		}
-
-		void insert(int index, T data)
-		{
-			if (mCapacity <= mSize + 1)
-			{
-				T* newData = new T[mCapacity];
-
-				for (int i = 0; i < index; ++i)
-				{
-					newData[i] = mData[i];
-				}
-
-				newData[index] = data;
-				mSize++;
-
-				for (int i = index + 1; i < mSize; ++i)
-				{
-					newData[i] = mData[i-1];
-				}
-
-				delete[] mData;
-				mData = newData;
-			}
-		}
-
-		void erase(T data)
-		{
-			int index = -1;
-			for (int i = 0; i < mSize; ++i)
-			{
 				if (mData[i] == data)
-				{
-					index = i;
-					break;
-				}
+					return i;
 			}
 
-			if (index != -1)
+			return -1;
+		}
+
+		// void insert(int index, T data)
+		// {
+		// 	if (mCapacity <= mSize + 1)
+		// 	{
+		// 		T* newData = new T[mCapacity];
+
+		// 		for (int i = 0; i < index; ++i)
+		// 		{
+		// 			newData[i] = mData[i];
+		// 		}
+
+		// 		newData[index] = data;
+		// 		mSize++;
+
+		// 		for (int i = index + 1; i < mSize; ++i)
+		// 		{
+		// 			newData[i] = mData[i-1];
+		// 		}
+
+		// 		delete[] mData;
+		// 		mData = newData;
+		// 	}
+		// }
+
+		void insert(iterator it, T data)
+		{
+			int index = it.get() - mData;
+
+			if (mSize >= mCapacity)
 			{
-				T* newData = new T[mCapacity];
-
-				for (int i = 0; i < index; ++i)
-				{
-					newData[i] = mData[i];
-				}
-
-				mSize--;
-
-				for (int i = index + 1; i < mSize; ++i)
-				{
-					newData[i] = mData[i+1];
-				}
-
-				delete[] mData;
-				mData = newData;
+				reserve(mCapacity * 2);
 			}
+
+			for (int i = mSize; i > index; --i)
+			{
+				mData[i] = mData[i - 1];
+			}
+
+			mData[index] = data;
+			++mSize;
+		}
+
+		// void erase(T data)
+		// {
+		// 	int index = -1;
+		// 	for (int i = 0; i < mSize; ++i)
+		// 	{
+		// 		if (mData[i] == data)
+		// 		{
+		// 			index = i;
+		// 			break;
+		// 		}
+		// 	}
+
+		// 	if (index != -1)
+		// 	{
+		// 		T* newData = new T[mCapacity];
+
+		// 		for (int i = 0; i < index; ++i)
+		// 		{
+		// 			newData[i] = mData[i];
+		// 		}
+
+		// 		mSize--;
+
+		// 		for (int i = index + 1; i < mSize; ++i)
+		// 		{
+		// 			newData[i] = mData[i+1];
+		// 		}
+
+		// 		delete[] mData;
+		// 		mData = newData;
+		// 	}
+		// }
+
+		void erase(iterator it)
+		{
+			int index = it.get() - mData;
+
+			for (int i = index; i < mSize - 1; ++i)
+			{
+				mData[i] = mData[i + 1];
+			}
+
+			--mSize;
 		}
 
 		void reverse()
@@ -153,7 +237,6 @@ class Vector
 
 		void clear()
 		{
-			delete[] mData;
 			mSize = 0;
 		}
 };
@@ -172,12 +255,26 @@ int main()
 		cout << "[" << i << "]" << container[i] << ",";
 	cout << endl;
 	//1.추가 2.삽입 3.삭제 4.모두삭제
-	for (int i = 0; i < container.size(); i++)
-		cout << container[i] << ",";
+	for (int i = 1; i < container.size(); ++i) container[i] = 10 + i * 10;
+	container.push_back(40);
+	Vector<int>::iterator it;
+
+	for (it = container.begin(); it != container.end(); it++)
+	{
+		if (*it == 40)
+		{
+			container.insert(it, 50);
+			break;
+		}
+	}
+
+	cout << "PrintPtr:";
+	for (it = container.begin(); it != container.end(); it++)
+		cout << "[" << &*it << "]" << *it << ",";
 	cout << endl;
 	container.clear(); //모두삭제
 	cout << "Clear:";
-	for (int i = 0; i < container.size(); i++)
-		cout << container[i] << ",";
+	for (it = container.begin(); it != container.end(); it++)
+		cout << "[" << &*it << "]" << *it << ",";
 	cout << endl;
 }
